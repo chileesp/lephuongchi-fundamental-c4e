8bbfb69f503ed469e9e52593649560b4ac09058e
chileesp <chilehnuehsgs@gmail.com>
2018-05-03 23:01:04 +0700

    homeworksess4

--- lab1/homework/timegmail.py
+++ lab1/homework/timegmail.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 from gmail import GMail, Message
 
-now = datetime.now()
-print (now)
+
+# print (now.day, now.month, now.year)
 
 while True:
+    now = datetime.now()
+    print(now)
     if now.hour >= 7:
         mail = GMail('rainbow.pc97@gmail.com', 'bong123456789')
         message = Message("Xin nghi om", to="chilehnuehsgs@gmail.com", text = "toi bi om roi")
